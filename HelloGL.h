85f596aecbde0d4c4a93822ce121c6779155c8d6
BigCelery842906 <84037295+BigCelery842906@users.noreply.github.com>
2025-03-07 15:23:17 +0000

    Initial commit

---
+++ HelloGL.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "GL/freeglut.h"
+
+class HelloGL
+{
+public:
+    
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "HelloGL.h"
+
+int main(int argc, char* argv[])
+{
+    HelloGL* game = new HelloGL();
+
+    return 0;
+
+}
