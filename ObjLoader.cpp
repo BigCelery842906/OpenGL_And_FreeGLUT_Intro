539fce936619f03e15577796e23a4333e15b07d4
BigCelery842906 <84037295+BigCelery842906@users.noreply.github.com>
2025-05-11 17:25:28 +0100

    Model Loads No Texture
    The model will load, however there is no texture or lighting applied

--- ObjLoader.cpp
+++ ObjLoader.cpp
@@ -142,98 +142,22 @@ namespace OBJ_Loader
         
         if (vertexCount > 0)
         {
-            for (int i = 0; i < objMesh.indexCount; i++)
+            for (int i = 0; i < objMesh.vertexCount; i++) //Vertexes are done perfectly
             {
-                int vIndex  = IndicesVector[i] - 1;
-                int vtIndex = texCoordIndicesVector[i] - 1;
-                int vnIndex = normalIndicesVector[i] - 1;
-
-                objMesh.Vertices[i] = VerticesVector[vIndex];
-
-                if (vtIndex >= 0 && vtIndex < TexCoordsVector.size())
-                    objMesh.TexCoords[i] = TexCoordsVector[vtIndex];
-
-                if (vnIndex >= 0 && vnIndex < NormalsVector.size())
-                    objMesh.Normals[i] = NormalsVector[vnIndex];
-
-                objMesh.Indices[i] = IndicesVector[i] - 1;
-
-            }
-            //     if (normalCount > 0)
-            //     {
-            //         objMesh.normalCount = normalCount;
-            //         objMesh.Normals = new Vector3[normalCount];
-            //         for (int i = 0; i < normalCount; i++)
-            //         {
-            //             objMesh.Normals[i].x = NormalsVector[normalIndicesVector[i]-1].x;
-            //             objMesh.Normals[i].y = NormalsVector[normalIndicesVector[i]-1].y;
-            //             objMesh.Normals[i].z = NormalsVector[normalIndicesVector[i]-1].z;
-            // std::cout << "Normal: " << objMesh.Normals[i].x << ", " << objMesh.Normals[i].y << ", " << objMesh.Normals[i].z << std::endl;
-            //         }
-            //     }
-            //     if (TexCoordCount > 0)
-            //     {
-            //         objMesh.TexCoordCount = texCoordIndicesVector.size();
-            // objMesh.TexCoords = new TexCoord[objMesh.indexCount];
-            //         for (int i = 0; i < objMesh.TexCoordCount; i++)
-            //         {
-            //             objMesh.TexCoords[i].u = TexCoordsVector[texCoordIndicesVector[i]-1].u;
-            // objMesh.TexCoords[i].v = TexCoordsVector[texCoordIndicesVector[i]-1].v;
-            // std::cout << "TexCoord: " << objMesh.TexCoords[i].u << ", " << objMesh.TexCoords[i].v << std::endl;
-            //         }
-            //     }
-            //     if (indexCount > 0)
-            //     {
-            //         objMesh.indexCount = indexCount;
-            //         objMesh.Indices = new GLushort[indexCount];
-            //         for (int i = 0; i < indexCount; i++)
-            //         {
-            //             objMesh.Indices[i] = IndicesVector[i]-1;
-            //         }
-        }
-    }
-
-    void ApplyIndexing(Mesh& objMesh)
-    {
-        if (vertexCount > 0)
-        {
-            objMesh.vertexCount = vertexCount;
-            objMesh.Vertices = new Vertex[vertexCount];
+                objMesh.Vertices[i].x = VerticesVector[i].x;
+                objMesh.Vertices[i].y = VerticesVector[i].y;
+                objMesh.Vertices[i].z = VerticesVector[i].z;
+                
+                //     int vIndex  = IndicesVector[i] - 1;
             
-            for (int i = 0; i < IndicesVector.size(); i++)
-            {
-                //int vertexIndexX = VerticesVector[i];
-                //objMesh.Vertices[i] = VerticesVector[vertexIndexX -1];
-                //int vertexIndexY = VerticesVector[i].y;
             }
         }
 
         if (TexCoordCount > 0)
         {
             objMesh.TexCoordCount = TexCoordCount;
-            objMesh.TexCoords = new TexCoord[TexCoordCount];
-            for (int i = 0; i < TexCoordsVector.size(); i++)
-            {
-                int TexVertexIndexU = TexCoordsVector[i].u;
-                objMesh.TexCoords[i].u = TexCoordsVector[TexVertexIndexU-1].u;
 
-                int TexVertexIndexV = TexCoordsVector[i].v;
-                objMesh.TexCoords[i].v = TexCoordsVector[TexVertexIndexV-1].v;
-            
-            }
-        }
-        if (normalCount > 0)
-        {
-            objMesh.normalCount = normalCount;
-            objMesh.Normals = new Vector3[normalCount];
-            for (int i = 0; i < NormalsVector.size(); i++)
             {
-                int vertexIndexX = NormalsVector[i].x;
-                objMesh.Normals[i].x = NormalsVector[vertexIndexX -1].x;
-                int vertexIndexY = NormalsVector[i].y;
-                objMesh.Normals[i].y = NormalsVector[vertexIndexY-1].y;
-                int vertexIndexZ = NormalsVector[i].z;
-                objMesh.Normals[i].z = NormalsVector[vertexIndexZ-1].z;
             }
         }
 
@@ -243,7 +167,7 @@ namespace OBJ_Loader
             objMesh.Indices = new GLushort[indexCount];
             for (int i = 0; i < indexCount; i++)
             {
-                objMesh.Indices[i] = i;
+                objMesh.Indices[i] = IndicesVector[i]-1;
             }
         }
     }
